87248e57952db567f031c708aa4fe9d6076ed2d5
Frank Chima Egwu <frankchimaegwu@gmail.com>
2022-04-20 19:52:23 -0700

    Added simple hello world web app

--- src/main/java/com/example/cdproject/CdprojectApplication.java
+++ src/main/java/com/example/cdproject/CdprojectApplication.java
@@ -1,10 +1,25 @@
 package com.example.cdproject;
 
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
+
 @SpringBootApplication
 public class CdprojectApplication {
+
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CdprojectApplication.class, args);
